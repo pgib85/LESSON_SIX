52882b2f4fd1c17546a240817cf6cc2d0a46ad4f
Parker Gibson <parker.gibson@slalom.com>
2026-04-14 08:42:58 -0500

    Added vuetify and material

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import './assets/main.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'light',
+  },
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
